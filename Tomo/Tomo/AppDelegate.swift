55e17764c5a7fae1d61fa5a44884bf31e06aa5b8
Xinguang Wang <admin@kansea.com>
2015-07-31 22:45:54 +0900

    fix

--- Tomo/Tomo/AppDelegate.swift
+++ Tomo/Tomo/AppDelegate.swift
@@ -25,10 +25,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         ApiController.setup()
         
-//        if Defaults["shouldAutoLogin"].bool == true {
+        if Defaults["shouldAutoLogin"].bool == true {
             let vc = Util.createViewControllerWithIdentifier("LoadingViewController", storyboardName: "Main")
             self.window?.rootViewController = vc
-//        }
+        }
         
         
         return true
