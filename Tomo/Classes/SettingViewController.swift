316f96d8e5c0eb3a7606ace134bd7fb432516945
Xinguang Wang <admin@kansea.com>
2015-08-27 15:08:02 +0900

    notification

--- Tomo/Classes/SettingViewController.swift
+++ Tomo/Classes/SettingViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SettingViewController: MyAccountBaseController {
+final class SettingViewController: MyAccountBaseController {
 
     @IBOutlet weak var fullNameLabel: UILabel!
     @IBOutlet weak var genderLabel: UILabel!
@@ -18,14 +18,49 @@ class SettingViewController: MyAccountBaseController {
     
     @IBOutlet weak var logoutCell: UITableViewCell!
     
+    @IBOutlet weak var notificationCollectionView: UICollectionView!
+    
+    var timer: NSTimer?
     var user: UserEntity!
+    var notifications: [NotificationEntity]? {
+        didSet {
+            var badge: String? = nil
+            if let notifications = notifications where notifications.count > 0 {
+                badge = String(notifications.count)
+                self.notificationCollectionView.reloadData()
+                if let timer = timer {
+                    timer.invalidate()
+                }
+                timer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: "nextNotification:", userInfo: nil, repeats: true)
+            } else {
+                if let timer = timer {
+                    timer.invalidate()
+                    self.timer = nil
+                }
+            }
+            self.parentViewController!.tabBarItem.badgeValue = badge
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Util.changeImageColorForButton(editButton,color: UIColor.whiteColor())        
+//        badgeLabel.layer.cornerRadius = badgeLabel.frame.size.height / 2
+        Util.changeImageColorForButton(editButton,color: UIColor.whiteColor())
         
         self.updateUI()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        Manager.sharedInstance.request(.GET, kAPIBaseURLString + "/mobile/notifications").validate().responseJSON { (_, _, result, error) -> Void in
+            if let result: AnyObject = result where error == nil {
+                if let array = JSON(result).array where array.count > 1 {
+                    self.notifications = array.map { return NotificationEntity($0.object) }
+                }
+            }
+        }
     }
     
     func updateUI() {
@@ -83,4 +118,83 @@ class SettingViewController: MyAccountBaseController {
         self.updateUI()
     }
 
+}
+
+// MARK - notificationCollectionView
+
+extension SettingViewController {
+    
+    func nextNotification(info: AnyObject){
+        if let notifications = self.notifications, paths = self.notificationCollectionView.indexPathsForSelectedItems() as? [NSIndexPath] where notifications.count > 0 {
+            
+            var path:NSIndexPath!
+            if paths.count > 0 {
+                self.notificationCollectionView.deselectItemAtIndexPath(paths[0], animated: true)
+                
+                let item = ( paths[0].item == notifications.count - 1 ) ? 0 : paths[0].item + 1
+                path = NSIndexPath(forItem: item, inSection: 0)
+                
+            } else {
+                path = NSIndexPath(forItem: 0, inSection: 0)
+            }
+            self.notificationCollectionView.selectItemAtIndexPath(path, animated: true, scrollPosition: .Top)
+//            self.notificationCollectionView.scrollToItemAtIndexPath(NSIndexPath(forItem: 1, inSection: 0), atScrollPosition: .Top, animated: true)
+        }
+    }
+}
+
+extension SettingViewController: UICollectionViewDataSource {
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.notifications?.count ?? 0
+    }
+    
+    // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let notification = self.notifications![indexPath.row]
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! UICollectionViewCell
+        
+        let imageView: AnyObject? = cell.subviews.first!.subviews.find { $0 is UIImageView }
+        let label: AnyObject? = cell.subviews.first!.subviews.find { $0 is UILabel }
+        
+        if let label = label as? UILabel, imageView = imageView as? UIImageView {
+            var actionString = "评论"
+            if let event = SocketEvent(rawValue: notification.type) {
+                if event == .PostCommented {
+                    actionString = "评论"
+                } else if event == .PostLiked {
+                    actionString = "赞"
+                }
+            }
+            if imageView.tag == 0 {
+                imageView.tag = 1
+                imageView.layer.cornerRadius = imageView.frame.size.width / 2
+                imageView.layer.masksToBounds = true
+            }
+            if let photo = notification.from.photo {
+                imageView.sd_setImageWithURL(NSURL(string: photo), placeholderImage: DefaultAvatarImage)
+            } else {
+                imageView.image = DefaultAvatarImage
+            }
+            label.text = "\(notification.from.nickName)\n    \(actionString)了您的帖子"
+        }
+        return cell
+    }
+}
+extension SettingViewController: UICollectionViewDelegate {
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        println(indexPath)
+    }
+    
+}
+
+extension SettingViewController {
+    
+    override func scrollViewDidScroll(scrollView: UIScrollView) {
+    
+        if scrollView == self.notificationCollectionView { return }
+        else { super.scrollViewDidScroll(scrollView) }
+    }
 }
