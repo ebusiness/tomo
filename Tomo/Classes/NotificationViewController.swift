101f85a7106eb94e2f171eb5b81deb72b5f4af7f
Xinguang Wang <admin@kansea.com>
2015-09-04 19:10:39 +0900

    NotificationViewController

---
+++ Tomo/Classes/NotificationViewController.swift
@@ -0,0 +1,49 @@
+//
+//  NotificationViewController.swift
+//  Tomo
+//
+//  Created by starboychina on 2015/09/04.
+//  Copyright (c) 2015年 &#24373;&#24535;&#33775;. All rights reserved.
+//
+
+
+import UIKit
+
+class NotificationViewController: MyAccountBaseController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+extension NotificationViewController: UITableViewDataSource {
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 60
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        return cell
+    }
+}
+
+extension NotificationViewController: UITableViewDelegate {
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        
+    }
+}
+
+
