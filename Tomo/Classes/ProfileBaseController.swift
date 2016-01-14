b0a6e2af0b4e533dd95c06bb4f2d0aecb053078d
Xinguang Wang <admin@kansea.com>
2016-01-14 20:22:43 +0900

    cover && post cell

--- Tomo/Classes/ProfileBaseController.swift
+++ Tomo/Classes/ProfileBaseController.swift
@@ -15,7 +15,7 @@ class ProfileBaseController: BaseTableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let resizeHeaderHeight:CGFloat = UIScreen.mainScreen().bounds.size.height * 0.618 + 80
+        let resizeHeaderHeight:CGFloat = UIScreen.mainScreen().bounds.size.height * 0.382 + 80
         self.headerHeight = resizeHeaderHeight - 80 - 64
         self.changeHeaderView(height: resizeHeaderHeight, done: nil)
     }
@@ -38,7 +38,7 @@ class ProfileBaseController: BaseTableViewController {
             }
             
             let maxHeight: CGFloat = 100
-            let speed: CGFloat = 0.5 * maxHeight / (UIScreen.mainScreen().bounds.size.height * 0.618 - 64) //{ return 0.5 * maxHeight / self.headerHeight }
+            let speed: CGFloat = 0.5 * maxHeight / (UIScreen.mainScreen().bounds.size.height * 0.382 - 64) //{ return 0.5 * maxHeight / self.headerHeight }
             var photoImageViewConstraints: Dictionary<NSLayoutAttribute, NSLayoutConstraint>?
             
             self.whenHideNavigationBar = { (OffsetY)->() in
